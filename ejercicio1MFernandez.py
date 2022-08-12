815e55aac3d6fff9eb2624f47916f55c883fd6c4
Maria Fernandez <mariiifer886@gmail.com>
2022-08-12 16:02:29 -0300

    ejercicio1

--- ejercicio1MFernandez.py
+++ ejercicio1MFernandez.py
@@ -1,1 +1,5 @@
-perro
+perro = [13, "Puppy", "12/12/2012", "Macho", 123]
+print("Contenido de la lista: ", perro)
+perro[2] = "13/12/2012"
+perro[4] = 28957346
+print("Nuevo contenido de la lista: ", perro)
